9c7243b4d217f83d8c48f5806b12e071b73d538c
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2014-03-11 17:34:06 -0400

    Added support for reading cuts to files.py. Changed the output format from having the ids in the same array as the data, to returning a tuple.

--- files.py
+++ files.py
@@ -1,11 +1,11 @@
 """This module provides low-level access to the actpol TOD metadata files."""
-import ast, numpy as np
+import ast, numpy as np, enlib.rangelist, re
 from enlib.utils import lines
 
 def read_gain(fname):
-	"""Reads per-detector gain values from file, returning [id,val]."""
+	"""Reads per-detector gain values from file, returning id,val."""
 	data = read_pylike_format(fname)
-	return np.vstack([data["det_uid"],data["cal"]])
+	return np.array(data["det_uid"]), np.array(data["cal"])
 
 def read_gain_correction(fname):
 	"""Reads per-tod overall gain correction from file. Returns
@@ -19,28 +19,31 @@ def read_gain_correction(fname):
 
 def read_polangle(fname):
 	"""Reads polarization angles in radians, discarding ones marked bad
-	(the negative ones). The format is returned as [id,val]."""
-	res = []
+	(the negative ones). The format is returned as id,val."""
+	ids, res = [], []
 	for line in lines(fname):
 		if line.startswith("#"): continue
 		toks = line.split()
 		id, ang = int(toks[0]), float(toks[3])*np.pi/180
 		if ang < 0: continue
-		res.append([id,ang])
-	return np.array(res).T
+		ids.append(id)
+		res.append(ang)
+	return np.array(ids), np.array(res)
 
 def read_tconst(fname):
 	"""Reads time constants from file, discarding those marked bad.
-	Returns format [id,val]"""
+	Returns format id,val"""
 	res = np.loadtxt(fname).T
-	return res[:2,res[2]==0]
+	res = res[:2,res[2]==0]
+	return res[0].astype(int), res[1]
 
-def read_point(fname):
-	"""Reads the per-detector pointing offsets, returning it in the form [id,dx,dy]."""
+def read_point_template(fname):
+	"""Reads the per-detector pointing offsets, returning it in the form id,[dx,dy]."""
 	res = np.loadtxt(fname, usecols=[0,1,2,4]).T
-	return res[:,res[1]>0][[0,2,3]]
+	res = res[:,res[1]>0][[0,2,3]]
+	return res[0].astype(int), res[1:]
 
-def read_point_offset(fname):
+def read_point_offsets(fname):
 	"""Reads per-tod pointing offsets, returning it in the form {todID: [dx,dy])."""
 	res = {}
 	for line in lines(fname):
@@ -48,6 +51,47 @@ def read_point_offset(fname):
 		toks = line.split()
 		res[toks[0]] = np.array([float(toks[5]),float(toks[6])])
 	return res
+
+def read_cut(fname):
+	"""Reads the act cut format, returning ids,cuts, where cuts is a Multirange
+	object."""
+	ids, cuts = [], []
+	header = re.compile(r"^(\w+) *= *(\w+)$")
+	rowcol = re.compile(r"^(\d+) +(\d+)$")
+	entry  = re.compile(r"^(?:.+ )?r(\d+)c(\d+):(.*)$")
+	nsamp  = 0
+	nmax   = 0
+	offset = 0
+	for line in lines(fname):
+		m = rowcol.match(line)
+		if m:
+			nrow, ncol = int(m.group(1)), int(m.group(2))
+			continue
+		m = header.match(line)
+		if m:
+			key, val = m.groups()
+			if key == "n_samp": nsamp = int(val)
+			elif key == "samp_offset": offset = int(val)
+			elif key == "n_row": nrow = int(val)
+			elif key == "n_col": ncol = int(val)
+			continue
+		m = entry.match(line)
+		if m:
+			r, c, toks = int(m.group(1)), int(m.group(2)), m.group(3).split()
+			id = r*ncol+c
+			ranges = np.array([[int(i) for i in word[1:-1].split(",")] for word in toks])
+			nmax   = max(nmax,max([sub[1] for sub in ranges]))
+			ids.append(id)
+			cuts.append(ranges)
+			continue
+	# If there is no cut information, assume *fully cut*. Also prune totally cut detectors
+	if nsamp == 0: nsamp = nmax
+	oids, ocuts = [], []
+	for id, cut in zip(ids, cuts):
+		if len(cut) > 1 or len(cut) == 1 and not (np.all(cut[0]==[0,0x7fffffff]) or np.all(cut[0]==[0,nsamp])):
+			oids.append(id)
+			ocuts.append(enlib.rangelist.Rangelist(cut,nsamp))
+	return oids, enlib.rangelist.Multirange(ocuts)
 
 def read_pylike_format(fname):
 	"""Givnen a file with a simple python-like format with lines of foo = [num,num,num,...],
