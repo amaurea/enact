052c9c967b7d90f60becb19c30c856eb35638574
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2015-07-14 10:24:29 -0400

    Implementing dynamic cuts. Commiting to move to laptop, since scinet is being slow

--- cuts.py
+++ cuts.py
@@ -1,11 +1,11 @@
 """This module implements extra, dynamic data cuts for ACT data."""
 import numpy as np
 from enlib.resample import resample_bin
-from enlib import rangelist, utils, config
+from enlib import rangelist, utils, config, coordinates, array_ops
 
-config.default("cut_turnaround_step", 20, "Smoothing length for turnaround cut")
-config.default("cut_turnaround_lim",   3, "Acceleration threshold for turnaround cut")
-config.default("cut_turnaround_margin",1, "Margin for turnaround cut in units of the smoothing length")
+config.default("cut_turnaround_step", 20, "Smoothing length for turnaround cut. Pointing will be downsampled by this number before acceleration is computed.")
+config.default("cut_turnaround_lim",   3, "Acceleration threshold for turnaround cut in units of standard deviations of the acceleration.")
+config.default("cut_turnaround_margin",1, "Margin for turnaround cut in units of the smoothing length. This will be added on each side of the acceleration-masked regions. In units of downsampled samples.")
 def turnaround_cut(t, az, step=None, lim=None, margin=None):
 	"""Cut samples where the telescope is accelerating."""
 	step   = config.get("cut_turnaround_step",   step)
@@ -21,7 +21,7 @@ def turnaround_cut(t, az, step=None, lim=None, margin=None):
 	r *= step
 	return rangelist.Rangelist(r, len(t))
 
-config.default("cut_ground_az", "58.75:60,-61:-59.75", "Az ranges to consider for ground cut")
+config.default("cut_ground_az", "57:62,-62:-57,73:75", "Az ranges to consider for ground cut")
 config.default("cut_ground_el", "0:38", "El ranges to consider for ground cut")
 def ground_cut(bore, det_offs, az_ranges=None, el_ranges=None):
 	az_ranges = np.array([[float(w) for w in tok.split(":")] for tok in config.get("cut_ground_az", az_ranges).split(",")])*np.pi/180
@@ -37,6 +37,32 @@ def ground_cut(bore, det_offs, az_ranges=None, el_ranges=None):
 		for er in el_ranges:
 			mask_el |= utils.between_angles(p[1], er)
 		cuts.append(rangelist.Rangelist(mask_az&mask_el))
+	return rangelist.Multirange(cuts)
+
+def avoidance_cut(bore, det_offs, site, name_or_pos, margin):
+	"""Cut samples that get too close to the specified object
+	(e.g. "Sun" or "Moon") or celestial position ([ra,dec] in racians).
+	Margin specifies how much to avoid the object by."""
+	cmargin = np.cos(margin)
+	mjd = utils.ctime2mjd(bore[0])
+	obj_pos  = coordinates.interpol_pos("cel","hor",name_or_pos,mjd,site)
+	obj_rect = utils.ang2rect(obj_pos, zenith=False)
+	# Only cut if above horizon
+	above_horizon = obj_pos[1]<0
+	above_horizon[:] = True
+	if np.all(~above_horizon): return rangelist.Multirange.empty(det_offs.shape[0], bore.shape[1])
+	cuts = []
+	for di, off in enumerate(det_offs):
+		det_pos  = bore[1:]+off[:,None]
+		#det_rect = utils.ang2rect(det_pos, zenith=False) # slow
+		det_rect = array_ops.ang2rect(det_pos)
+		cdist = np.sum(obj_rect*det_rect,0)
+		if di == 0:
+			print det_pos, obj_pos
+			print cdist[0], cmargin, above_horizon[0], np.where(above_horizon)
+		# Cut samples above horizon that are too close
+		bad  = (cdist > cmargin) & above_horizon
+		cuts.append(rangelist.Rangelist(bad))
 	return rangelist.Multirange(cuts)
 
 def test_cut(bore, frac=0.3, dfrac=0.05):
