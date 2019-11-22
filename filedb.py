b71830e9ed342025a5d37b7766e58b6ee813248d
Sigurd Kirkevold Næss <sigurdkn@astro.uio.no>
2019-11-22 16:21:24 -0500

    Made get_patch_path pass through filenames that end with fits directly

--- filedb.py
+++ filedb.py
@@ -51,7 +51,8 @@ def setup_filedb():
 def cjoin(names): return os.path.join(*[config.get(n) for n in names])
 
 def get_patch_path(name):
-	return os.path.join(config.get("root"), config.get("patch_dir"), name)+".fits"
+	if name.endswith(".fits"): return name
+	else: return os.path.join(config.get("root"), config.get("patch_dir"), name)+".fits"
 
 def init():
 	global scans, data
